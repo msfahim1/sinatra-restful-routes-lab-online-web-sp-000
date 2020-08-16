c5f31c1dee422b621386013906f7807d885f9407
Michael Fahim <michael_fahim@yahoo.com>
2020-08-16 00:16:45 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,90 +1,84 @@
 class TweetsController < ApplicationController
+
   get '/tweets' do
-    if logged_in?
-      @tweets = Tweet.all
-      erb :'tweets/tweets'
-    else
-      redirect to '/login'
+    if !Helpers.logged_in?(session)
+      redirect '/login'
     end
+    @tweets = Tweet.all
+    @user = Helpers.current_user(session)
+    erb :'/tweets/tweets'
+  end
+
+  # shows all tweets from a single user
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
   end
 
   get '/tweets/new' do
-    if logged_in?
-      erb :'tweets/create_tweet'
-    else
-      redirect to '/login'
+    if !Helpers.logged_in?(session)
+      flash[:message] = "You Must Be Logged in to Make Tweets"
+      redirect '/login'
     end
+      erb :'tweets/new'
   end
 
+  # The tweet should be created and saved to the database
   post '/tweets' do
-    if logged_in?
-      if params[:content] == ""
-        redirect to "/tweets/new"
-      else
-        @tweet = current_user.tweets.build(content: params[:content])
-        if @tweet.save
-          redirect to "/tweets/#{@tweet.id}"
-        else
-          redirect to "/tweets/new"
-        end
-      end
+    if !params[:content].empty?
+      Tweet.create(content: params[:content], user_id: session[:user_id])
+      redirect '/tweets'
     else
-      redirect to '/login'
+      flash[:error] = "Please enter a tweet"
+      redirect '/tweets/new'
     end
   end
 
   get '/tweets/:id' do
-    if logged_in?
-      @tweet = Tweet.find_by_id(params[:id])
-      erb :'tweets/show_tweet'
+    if !Helpers.logged_in?(session)
+      flash[:message] = "You Must Be Logged in to View Tweets"
+      redirect '/login'
     else
-      redirect to '/login'
+      @tweet = Tweet.find_by(params[:id])
+      erb :'/tweets/show_tweet'
     end
   end
 
   get '/tweets/:id/edit' do
-    if logged_in?
-      @tweet = Tweet.find_by_id(params[:id])
-      if @tweet && @tweet.user == current_user
-        erb :'tweets/edit_tweet'
-      else
-        redirect to '/tweets'
-      end
+    if !Helpers.logged_in?(session)
+      flash[:message] = "You Must Be Logged in to Edit Tweets"
+      redirect '/login'
     else
-      redirect to '/login'
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/edit_tweet'
     end
   end
 
-  patch '/tweets/:id' do
-    if logged_in?
-      if params[:content] == ""
-        redirect to "/tweets/#{params[:id]}/edit"
-      else
-        @tweet = Tweet.find_by_id(params[:id])
-        if @tweet && @tweet.user == current_user
-          if @tweet.update(content: params[:content])
-            redirect to "/tweets/#{@tweet.id}"
-          else
-            redirect to "/tweets/#{@tweet.id}/edit"
-          end
-        else
-          redirect to '/tweets'
-        end
-      end
+  # lets a user edit their own tweet if they are logged in
+  # does not let a user edit a text with blank content
+  post '/tweets/:id' do
+    @tweet = Tweet.find_by(params[:user_id])
+    if !params[:content].empty?
+      @tweet.update(content: params[:content])
+      redirect "tweets/#{@tweet.id}"
     else
-      redirect to '/login'
+      flash[:message] = "Please fill in the text field to Tweet"
+      redirect "tweets/#{@tweet.id}/edit"
     end
   end
 
-  delete '/tweets/:id/delete' do
-    if logged_in?
-      @tweet = Tweet.find_by_id(params[:id])
-      if @tweet && @tweet.user == current_user
-        @tweet.delete
-      end
-      redirect to '/tweets'
+  # lets a user delete their own tweet if they are logged in
+  # does not let a user delete a tweet they did not create
+  post '/tweets/:id/delete' do
+    @tweet = Tweet.find_by(params[:user_id])
+    if !Helpers.logged_in?(session)
+      flash[:message] = "Log in to your account to delete Tweets"
+      redirect '/login'
     else
-      redirect to '/login'
+      @tweet.delete
+      redirect '/tweets'
     end
   end
+
+
 end
