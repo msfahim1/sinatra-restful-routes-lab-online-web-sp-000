4d71e798d53496e674dfebcaf370fd09fe152971
Michael Fahim <michael_fahim@yahoo.com>
2020-08-16 00:16:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,21 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+
+  use Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "fwitter_secret"
+    use Rack::Flash
+    set :session_secret, "password_security"
   end
 
+  # load homepage for sign up and login
   get '/' do
     erb :index
-  end
-
-  helpers do
-    def logged_in?
-      !!current_user
-    end
-
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    end
   end
 
 end
