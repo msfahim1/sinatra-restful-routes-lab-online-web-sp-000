13ecbf7dbb220ef02ab5bde9b16d7a50b0e8c63a
Michael Fahim <michael_fahim@yahoo.com>
2020-08-16 00:17:15 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ActiveRecord::Base
-  has_secure_password
   has_many :tweets
+  has_secure_password
 
   def slug
-    username.downcase.gsub(' ', '-')
+    self.username.gsub(' ', '-').downcase if self.username
   end
 
   def self.find_by_slug(slug)
-    User.all.find{ |user| user.slug == slug }
+    self.all.find do |result|
+      result.slug == slug
+    end
   end
+
 end
